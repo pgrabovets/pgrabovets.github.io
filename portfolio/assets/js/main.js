2d225b5c7c59e3d62bd5eefb538c9a348c79840a
Pavel <pgrabovets3056@gmail.com>
2019-10-08 23:37:24 +0300

    Delete unnecessary pages

--- portfolio/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-    var masonry = new Masonry('.grid', {
-        itemSelector: '.grid-item',
-    });
-    var grid = document.querySelector('.grid');
-    grid.style.visibility = 'visible';
-});
